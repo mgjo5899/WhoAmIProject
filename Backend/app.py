bec6aaedf8c19351180770b8a9abd5c764448ca4
mgjo5899 <mgjo5899@gmail.com>
2018-05-31 22:29:48 -0500

    Changed response retrieval part

--- Backend/app.py
+++ Backend/app.py
@@ -3,21 +3,20 @@ from flask import jsonify
 from flask import request
 from flask_cors import CORS
 import models as dbHandler
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/register', methods=['POST'])
 def home():
-    if request.method=='POST':
-        username = request.form['username']
-        password = request.form['password']
-        dbHandler.register_user(username, password)
-        users = dbHandler.retrieve_users()
+    req = json.loads(request.data.decode("utf-8"))
+    username = req['username']
+    password = req['password']
+    dbHandler.register_user(username, password)
+    users = dbHandler.retrieve_users()
 
-        return jsonify(users)
-    else:
-        return jsonify(errcode="wrong_request_method")
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
