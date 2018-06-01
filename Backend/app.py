953a081221346b5ef224be2c5cf8257134ba59a9
mgjo5899 <mgjo5899@gmail.com>
2018-05-31 20:30:29 -0500

    Added CORS to Flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_cors import CORS
 import models as dbHandler
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/register', methods=['POST'])
 def home():
