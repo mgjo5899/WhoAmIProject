fa7f001b5a61fe0ec95a9657e5b134785ddc0620
ykim164 <ykim164@illinois.edu>
2018-05-31 19:45:51 -0500

    set up material-ui

--- Frontend/src/common/TextFieldGroup.jsx
+++ Frontend/src/common/TextFieldGroup.jsx
@@ -24,6 +24,10 @@ TextFieldGroup.propTypes = {
 	type: React.PropTypes.string.isRequired,
 	onChange: React.PropTypes.func.isRequired,
 }
+
+만약에 여러분이 만들 컴포넌트가 라이프사이클 API 도 사용하지 않고, 
+state 도 사용하지 않고, 그냥 props 만 전달해주면 뷰를 렌더링만 해주는 
+역할이라면 함수형 컴포넌트 형식으로 컴포넌트를 정의 할 수 있습니다. 
 */
 
 TextFieldGroup.defaultProps = {
