3ffc753137097ae903fd990f8aaba6e772156269
mgjo5899 <mgjo5899@gmail.com>
2018-06-03 17:38:01 -0500

    Backend now supports both react and postman requests

--- Backend/app.py
+++ Backend/app.py
@@ -8,15 +8,40 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET', 'DELETE'])
 def home():
-    req = json.loads(request.data.decode("utf-8"))
-    username = req['username']
-    password = req['password']
-    dbHandler.register_user(username, password)
-    users = dbHandler.retrieve_users()
+    if request.method == 'POST':
+        if len(request.form) > 0:
+            # Postman
+            # request.data is empty with Postman
+            print("request form: " + str(request.form))
+            username = request.form['username']
+            password = request.form['password']
+        else:
+            # React
+            # request.form is empty with react
+            req = json.loads(request.data.decode("utf-8"))
+            print("request data: " + str(req))
+            username = req['username']
+            password = req['password']
 
-    return jsonify(users)
+        dbHandler.register_user(username, password)
+        users = dbHandler.retrieve_users()
+        rtn_val = {}
+        rtn_val['status'] = "Success"
+        rtn_val['users'] = users
+
+        return jsonify(rtn_val)
+    elif request.method == 'GET':
+        users = dbHandler.retrieve_users()
+        
+        return jsonify(users)
+    elif request.method == 'DELETE':
+        return jsonify({'error':"Wrong request"})
+    else:
+        # Usually it gets handled automatically by Flask
+        return jsonify({'error':"Wrong request"})
+        
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
