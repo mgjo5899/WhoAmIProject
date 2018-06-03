0c119f8270bd46060a4af0414db6f46bb3eeb331
mgjo5899 <mgjo5899@gmail.com>
2018-06-03 18:33:39 -0500

    Supports PUT, DELETE, and GET ; changed from register to users

--- Backend/app.py
+++ Backend/app.py
@@ -8,9 +8,39 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/register', methods=['POST', 'GET', 'DELETE'])
+@app.route('/users', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def home():
-    if request.method == 'POST':
+    if request.method == 'PUT':
+        if len(request.form) > 0:
+            # Postman
+            # request.data is empty with Postman
+            print("request form: " + str(request.form))
+            username = request.form['username']
+            password = request.form['password']
+            new_password = request.form['new_password']
+        else:
+            # React
+            # request.form is empty with react
+            req = json.loads(request.data.decode("utf-8"))
+            print("request data: " + str(req))
+            username = req['username']
+            password = req['password']
+            new_password = req['new_password']
+
+        rtn_val = {}
+
+        if not dbHandler.modify_user(username, password, new_password):
+            rtn_val['status'] = "Fail"
+            rtn_val['error'] = "There is no user with the given username"
+        else:
+            rtn_val['status'] = "Success"
+
+        users = dbHandler.retrieve_users()
+        rtn_val['current users'] = users
+
+        return jsonify(rtn_val)
+            
+    elif request.method == 'POST' or request.method == 'DELETE':
         if len(request.form) > 0:
             # Postman
             # request.data is empty with Postman
@@ -25,23 +55,32 @@ def home():
             username = req['username']
             password = req['password']
 
-        dbHandler.register_user(username, password)
+        rtn_val = {}
+
+        if request.method == 'POST':
+            if not dbHandler.register_user(username, password):
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "There is a user with the given username"
+            else:
+                rtn_val['status'] = "Success"
+        elif  request.method == 'DELETE':
+            if not dbHandler.delete_user(username, password):
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "There is no user with the given username"
+            else:
+                rtn_val['status'] = "Success"
+        users = dbHandler.retrieve_users()
+        rtn_val['current users'] = users
+
+        return jsonify(rtn_val)
+
+    elif request.method == 'GET':
         users = dbHandler.retrieve_users()
         rtn_val = {}
         rtn_val['status'] = "Success"
         rtn_val['users'] = users
-
+        
         return jsonify(rtn_val)
-    elif request.method == 'GET':
-        users = dbHandler.retrieve_users()
-        
-        return jsonify(users)
-    elif request.method == 'DELETE':
-        return jsonify({'error':"Wrong request"})
-    else:
-        # Usually it gets handled automatically by Flask
-        return jsonify({'error':"Wrong request"})
-        
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- Backend/models.py
+++ Backend/models.py
@@ -1,11 +1,25 @@
 import sqlite3 as sql
 
-def register_user(username,password):
+def check_user(username):
+    con = sql.connect("db/database.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE username = (?)", (username,))
+    user = cur.fetchall()
+    con.close()
+
+    return user
+
+def register_user(username, password):
+    if len(check_user(username)) != 0:
+        return False
+
     con = sql.connect("db/database.db")
     cur = con.cursor()
     cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
     con.commit()
     con.close()
+
+    return True
 
 def retrieve_users():
 	con = sql.connect("db/database.db")
@@ -13,4 +27,29 @@ def retrieve_users():
 	cur.execute("SELECT username, password FROM users")
 	users = cur.fetchall()
 	con.close()
+
 	return users
+
+def delete_user(username, password):
+    if len(check_user(username)) == 0:
+        return False
+
+    con = sql.connect("db/database.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM users WHERE username = ? AND password = ?", (username,password))
+    con.commit()
+    con.close()
+
+    return True
+
+def modify_user(username, password, new_password):
+    if len(check_user(username)) == 0:
+        return False
+
+    con = sql.connect("db/database.db")
+    cur = con.cursor()
+    cur.execute("UPDATE users SET password = ? WHERE username = ? AND password = ?", (new_password, username, password))
+    con.commit()
+    con.close()
+
+    return True
