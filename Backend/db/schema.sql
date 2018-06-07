5b98420e0c595d0edd3994f82b7a50f2be90ae4c
mgjo5899 <mgjo5899@gmail.com>
2018-06-06 21:46:55 -0500

    Added two password related columns in table into one

--- Backend/db/schema.sql
+++ Backend/db/schema.sql
@@ -3,6 +3,5 @@ drop table if exists users;
     id integer primary key autoincrement,
     username text not null,
     email text not null,
-    password_salt text not null,
-    password_hash text not null
+    password text not null
 );
--- Backend/models.py
+++ Backend/models.py
@@ -6,7 +6,7 @@ USER_DB = "db/userdata.db"
 def check_user(username):
     con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("SELECT username, password_salt, password_hash \
+    cur.execute("SELECT username, password \
                  FROM users WHERE username = (?)", (username,))
     user = cur.fetchall()
     con.close()
@@ -18,15 +18,13 @@ def register_user(username, password, email):
         return False
 
     # Hashing
-    rtn_str = sec.generate_password_hash(password, method='sha256')[7:]
-    password_salt = rtn_str[:rtn_str.find('$')]
-    password_hash = rtn_str[rtn_str.find('$') + 1:]
+    hashed_pw = sec.generate_password_hash(password, method='sha256')[7:]
 
     con = sql.connect(USER_DB)
     cur = con.cursor()
     cur.execute("INSERT INTO users \
-                (username, email, password_salt, password_hash) VALUES (?,?,?,?)", \
-                (username, email, password_salt, password_hash))
+                (username, email, password) VALUES (?,?,?)", \
+                (username, email, hashed_pw))
     con.commit()
     con.close()
 
@@ -48,7 +46,7 @@ def delete_user(username, password):
         return False
 
     user = user[0]
-    hash_str = 'sha256$' + user[1] + '$' + user[2]
+    hash_str = 'sha256$' + user[1]
 
     if not sec.check_password_hash(hash_str, password):
         return False
@@ -69,21 +67,19 @@ def modify_user(username, password, new_password):
 
     user = user[0]
     print(user)
-    hash_str = 'sha256$' + user[1] + '$' + user[2]
+    hash_str = 'sha256$' + user[1]
 
     if not sec.check_password_hash(hash_str, password):
         print('wrong password')
         return False
 
     # Hashing
-    rtn_str = sec.generate_password_hash(new_password, method='sha256')[7:]
-    password_salt = rtn_str[:rtn_str.find('$')]
-    password_hash = rtn_str[rtn_str.find('$') + 1:]
+    hashed_pw = sec.generate_password_hash(new_password, method='sha256')[7:]
 
     con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("UPDATE users SET password_salt = ?, password_hash = ? \
-                 WHERE username = ?", (password_salt, password_hash, username))
+    cur.execute("UPDATE users SET password = ? \
+                 WHERE username = ?", (hashed_pw, username))
     con.commit()
     con.close()
 
