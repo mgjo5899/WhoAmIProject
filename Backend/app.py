b15177a0626a2daa709013aa326405f573a64e44
mgjo5899 <mgjo5899@gmail.com>
2018-06-06 21:20:06 -0500

    Catching errors in the requests; added hashing mechanism for password in models.py

--- Backend/app.py
+++ Backend/app.py
@@ -10,67 +10,120 @@ CORS(app)
 
 @app.route('/users', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def home():
+    rtn_val = {}
+
     if request.method == 'PUT':
         if len(request.form) > 0:
             # Postman
             # request.data is empty with Postman
             print("request form: " + str(request.form))
-            username = request.form['username']
-            password = request.form['password']
-            new_password = request.form['new_password']
+
+            try:
+                username = request.form['username']
+                password = request.form['password']
+                new_password = request.form['new_password']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username, password, or new_password"
         else:
             # React
             # request.form is empty with react
             req = json.loads(request.data.decode("utf-8"))
             print("request data: " + str(req))
-            username = req['username']
-            password = req['password']
-            new_password = req['new_password']
 
-        rtn_val = {}
+            try:
+                username = req['username']
+                password = req['password']
+                new_password = req['new_password']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username, password, or new_password"
 
-        if not dbHandler.modify_user(username, password, new_password):
-            rtn_val['status'] = "Fail"
-            rtn_val['error'] = "There is no user with the given username"
-        else:
-            rtn_val['status'] = "Success"
+        if 'error' not in rtn_val:
+            if not dbHandler.modify_user(username, password, new_password):
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "There is no user with the given username and password"
+            else:
+                rtn_val['status'] = "Success"
 
-        users = dbHandler.retrieve_users()
-        rtn_val['current users'] = users
+            users = dbHandler.retrieve_users()
+            rtn_val['current users'] = users
 
         return jsonify(rtn_val)
             
-    elif request.method == 'POST' or request.method == 'DELETE':
+    elif request.method == 'POST':
         if len(request.form) > 0:
             # Postman
             # request.data is empty with Postman
             print("request form: " + str(request.form))
-            username = request.form['username']
-            password = request.form['password']
+
+            try:
+                username = request.form['username']
+                password = request.form['password']
+                email = request.form['email']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username, password, or email"
         else:
             # React
             # request.form is empty with react
             req = json.loads(request.data.decode("utf-8"))
             print("request data: " + str(req))
-            username = req['username']
-            password = req['password']
 
-        rtn_val = {}
+            try:
+                username = req['username']
+                password = req['password']
+                email = req['email']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username, password, or email"
 
-        if request.method == 'POST':
-            if not dbHandler.register_user(username, password):
+        if 'error' not in rtn_val:
+            if not dbHandler.register_user(username, password, email):
                 rtn_val['status'] = "Fail"
                 rtn_val['error'] = "There is a user with the given username"
             else:
                 rtn_val['status'] = "Success"
-        elif  request.method == 'DELETE':
+
+            users = dbHandler.retrieve_users()
+            rtn_val['current users'] = users
+
+        return jsonify(rtn_val)
+
+    elif request.method == 'DELETE':
+        if len(request.form) > 0:
+            # Postman
+            # request.data is empty with Postman
+            print("request form: " + str(request.form))
+
+            try:
+                username = request.form['username']
+                password = request.form['password']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username or password"
+        else:
+            # React
+            # request.form is empty with react
+            req = json.loads(request.data.decode("utf-8"))
+            print("request data: " + str(req))
+
+            try:
+                username = req['username']
+                password = req['password']
+            except:
+                rtn_val['status'] = "Fail"
+                rtn_val['error'] = "Request is missing either username or password"
+
+        if 'error' not in rtn_val:
             if not dbHandler.delete_user(username, password):
                 rtn_val['status'] = "Fail"
                 rtn_val['error'] = "There is no user with the given username"
             else:
                 rtn_val['status'] = "Success"
-        users = dbHandler.retrieve_users()
-        rtn_val['current users'] = users
+
+            users = dbHandler.retrieve_users()
+            rtn_val['current users'] = users
 
         return jsonify(rtn_val)
 
--- Backend/models.py
+++ Backend/models.py
@@ -1,54 +1,89 @@
 import sqlite3 as sql
+import werkzeug.security as sec
+
+USER_DB = "db/userdata.db"
 
 def check_user(username):
-    con = sql.connect("db/database.db")
+    con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE username = (?)", (username,))
+    cur.execute("SELECT username, password_salt, password_hash \
+                 FROM users WHERE username = (?)", (username,))
     user = cur.fetchall()
     con.close()
 
     return user
 
-def register_user(username, password):
+def register_user(username, password, email):
     if len(check_user(username)) != 0:
         return False
 
-    con = sql.connect("db/database.db")
+    # Hashing
+    rtn_str = sec.generate_password_hash(password, method='sha256')[7:]
+    password_salt = rtn_str[:rtn_str.find('$')]
+    password_hash = rtn_str[rtn_str.find('$') + 1:]
+
+    con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
+    cur.execute("INSERT INTO users \
+                (username, email, password_salt, password_hash) VALUES (?,?,?,?)", \
+                (username, email, password_salt, password_hash))
     con.commit()
     con.close()
 
     return True
 
 def retrieve_users():
-	con = sql.connect("db/database.db")
+	con = sql.connect(USER_DB)
 	cur = con.cursor()
-	cur.execute("SELECT username, password FROM users")
+	cur.execute("SELECT username, email FROM users")
 	users = cur.fetchall()
 	con.close()
 
 	return users
 
 def delete_user(username, password):
-    if len(check_user(username)) == 0:
+    user = check_user(username)
+
+    if len(user) == 0:
         return False
 
-    con = sql.connect("db/database.db")
+    user = user[0]
+    hash_str = 'sha256$' + user[1] + '$' + user[2]
+
+    if not sec.check_password_hash(hash_str, password):
+        return False
+    
+    con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("DELETE FROM users WHERE username = ? AND password = ?", (username,password))
+    cur.execute("DELETE FROM users WHERE username = (?)", (username,))
     con.commit()
     con.close()
 
     return True
 
 def modify_user(username, password, new_password):
-    if len(check_user(username)) == 0:
+    user = check_user(username)
+
+    if len(user) == 0:
         return False
 
-    con = sql.connect("db/database.db")
+    user = user[0]
+    print(user)
+    hash_str = 'sha256$' + user[1] + '$' + user[2]
+
+    if not sec.check_password_hash(hash_str, password):
+        print('wrong password')
+        return False
+
+    # Hashing
+    rtn_str = sec.generate_password_hash(new_password, method='sha256')[7:]
+    password_salt = rtn_str[:rtn_str.find('$')]
+    password_hash = rtn_str[rtn_str.find('$') + 1:]
+
+    con = sql.connect(USER_DB)
     cur = con.cursor()
-    cur.execute("UPDATE users SET password = ? WHERE username = ? AND password = ?", (new_password, username, password))
+    cur.execute("UPDATE users SET password_salt = ?, password_hash = ? \
+                 WHERE username = ?", (password_salt, password_hash, username))
     con.commit()
     con.close()
 
