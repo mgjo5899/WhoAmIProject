ac10d4627cc7a33907c218c0d276f83e9b9b4c80
mgjo5899 <mgjo5899@gmail.com>
2018-06-06 21:19:35 -0500

    Took out raw password, put in salted password mechanism; also added email in the table

--- Backend/db/schema.sql
+++ Backend/db/schema.sql
@@ -2,5 +2,7 @@ drop table if exists users;
     create table users (
     id integer primary key autoincrement,
     username text not null,
-    password text not null
+    email text not null,
+    password_salt text not null,
+    password_hash text not null
 );
