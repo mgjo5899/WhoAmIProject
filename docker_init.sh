2e4c72e304f1f9b07444ac489d4064df104e3077
mgjo5899 <mgjo5899@gmail.com>
2018-06-06 21:13:16 -0500

    Added mechanism to stop docker_init.sh if there's no docker running

--- docker_init.sh
+++ docker_init.sh
@@ -1,4 +1,18 @@
-docker stop $(docker ps -q -a)
-docker rm $(docker ps -q -a)
+DOCKER_PROCESS=$(docker ps | head -1)
+
+if [ ${DOCKER_PROCESS:0:9} == "CONTAINER" ]
+then
+    echo "Docker is up!"
+
+    echo "docker stop all"
+    docker stop $(docker ps -q -a)
+
+    echo "docker rm all"
+    docker rm $(docker ps -q -a)
+
+    echo "docker build backend"
+    docker build -t backend .
+fi
+
+###############################
 #docker rmi $(docker images -q)
-docker build -t backend .
