307dbf40fcefc65e386350c539d6c307f852ac84
mgjo5899 <mgjo5899@gmail.com>
2018-06-10 16:50:56 -0500

    refactored code of app.py; added signin api

--- Backend/app.py
+++ Backend/app.py
@@ -8,77 +8,80 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/signin', methods=['POST'])
+def signin():
+    rtn_val = {}
+
+    if len(request.form) > 0:
+        # Postman
+        # request.data is empty with Postman
+        print("Getting data from request.form")
+        req = request.form
+    elif len(request.data) > 0:
+        # React
+        # request.form is empty with react
+        print("Getting data from request.data")
+        req = json.loads(request.data.decode("utf-8"))
+
+    if 'username' in req and 'password' in req:
+        username = req['username']
+        password = req['password']
+
+        if not dbHandler.signin_user(username, password):
+            rtn_val['status'] = "Fail"
+            rtn_val['error'] = "There is no user with the given username and password"
+        else:
+            rtn_val['status'] = "Success"
+    else:
+        rtn_val['status'] = "Fail"
+        rtn_val['error'] = "Request is missing either username or password"
+
+    return jsonify(rtn_val)
+
+
 @app.route('/users', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def home():
     rtn_val = {}
 
+    if len(request.form) > 0:
+        # Postman
+        # request.data is empty with Postman
+        print("Getting data from request.form")
+        req = request.form
+    elif len(request.data) > 0:
+        # React
+        # request.form is empty with react
+        print("Getting data from request.data")
+        req = json.loads(request.data.decode("utf-8"))
+    else:
+        print("Possibly GET request")
+
     if request.method == 'PUT':
-        if len(request.form) > 0:
-            # Postman
-            # request.data is empty with Postman
-            print("request form: " + str(request.form))
+        if 'username' in req and 'password' in req and 'new_password' in req:
+            username = req['username']
+            password = req['password']
+            new_password = req['new_password']
 
-            try:
-                username = request.form['username']
-                password = request.form['password']
-                new_password = request.form['new_password']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username, password, or new_password"
-        else:
-            # React
-            # request.form is empty with react
-            req = json.loads(request.data.decode("utf-8"))
-            print("request data: " + str(req))
-
-            try:
-                username = req['username']
-                password = req['password']
-                new_password = req['new_password']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username, password, or new_password"
-
-        if 'error' not in rtn_val:
             if not dbHandler.modify_user(username, password, new_password):
                 rtn_val['status'] = "Fail"
-                rtn_val['error'] = "There is no user with the given username and password"
+                rtn_val['error'] = "There is no user with the given username and \
+                                                                         password"
             else:
                 rtn_val['status'] = "Success"
 
             users = dbHandler.retrieve_users()
             rtn_val['current users'] = users
-
-        return jsonify(rtn_val)
+        else:
+            rtn_val['status'] = "Fail"
+            rtn_val['error'] = "Request is missing either username, password, \
+                                                                   or new_password"
             
     elif request.method == 'POST':
-        if len(request.form) > 0:
-            # Postman
-            # request.data is empty with Postman
-            print("request form: " + str(request.form))
+        if 'username' in req and 'password' in req and 'email' in req:
+            username = req['username']
+            password = req['password']
+            email = req['email']
 
-            try:
-                username = request.form['username']
-                password = request.form['password']
-                email = request.form['email']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username, password, or email"
-        else:
-            # React
-            # request.form is empty with react
-            req = json.loads(request.data.decode("utf-8"))
-            print("request data: " + str(req))
-
-            try:
-                username = req['username']
-                password = req['password']
-                email = req['email']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username, password, or email"
-
-        if 'error' not in rtn_val:
             if not dbHandler.register_user(username, password, email):
                 rtn_val['status'] = "Fail"
                 rtn_val['error'] = "There is a user with the given username"
@@ -87,35 +90,15 @@ def home():
 
             users = dbHandler.retrieve_users()
             rtn_val['current users'] = users
-
-        return jsonify(rtn_val)
+        else:
+            rtn_val['status'] = "Fail"
+            rtn_val['error'] = "Request is missing either username, password, or email"
 
     elif request.method == 'DELETE':
-        if len(request.form) > 0:
-            # Postman
-            # request.data is empty with Postman
-            print("request form: " + str(request.form))
+        if 'username' in req and 'password' in req:
+            username = req['username']
+            password = req['password']
 
-            try:
-                username = request.form['username']
-                password = request.form['password']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username or password"
-        else:
-            # React
-            # request.form is empty with react
-            req = json.loads(request.data.decode("utf-8"))
-            print("request data: " + str(req))
-
-            try:
-                username = req['username']
-                password = req['password']
-            except:
-                rtn_val['status'] = "Fail"
-                rtn_val['error'] = "Request is missing either username or password"
-
-        if 'error' not in rtn_val:
             if not dbHandler.delete_user(username, password):
                 rtn_val['status'] = "Fail"
                 rtn_val['error'] = "There is no user with the given username"
@@ -124,16 +107,17 @@ def home():
 
             users = dbHandler.retrieve_users()
             rtn_val['current users'] = users
-
-        return jsonify(rtn_val)
+        else:
+            rtn_val['status'] = "Fail"
+            rtn_val['error'] = "Request is missing either username or password"
 
     elif request.method == 'GET':
         users = dbHandler.retrieve_users()
         rtn_val = {}
         rtn_val['status'] = "Success"
         rtn_val['users'] = users
-        
-        return jsonify(rtn_val)
+
+    return jsonify(rtn_val)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
