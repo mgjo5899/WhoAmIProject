9d2bdc11194a110784182757dc344fe92f297879
mgjo5899 <mgjo5899@gmail.com>
2018-07-02 01:06:53 -0500

    Now usernames and emails have to be unique; so it's applied in test_data.py

--- backend/test_data.py
+++ backend/test_data.py
@@ -7,15 +7,15 @@ utils.db_checks()
 # Populating fake user table
 users = {
             'joseph':['mgjo5899@gmail.com', '1000'],
-            'cho':['mgjo5899@gmail.com', '1000'],
-            'kim':['mgjo5899@gmail.com', '1000'],
-            'jo':['mgjo5899@gmail.com', '1000'],
-            'yj':['mgjo5899@gmail.com', '1000'],
-            'yjk':['mgjo5899@gmail.com', '1000'],
-            'jocho':['mgjo5899@gmail.com', '1000'],
-            'lee':['mgjo5899@gmail.com', '1000'],
-            'park':['mgjo5899@gmail.com', '1000'],
-            'mkc':['mgjo5899@gmail.com', '1000']
+            'cho':['mgjo58991@gmail.com', '1000'],
+            'kim':['mgjo58992@gmail.com', '1000'],
+            'jo':['mgjo58993@gmail.com', '1000'],
+            'yj':['mgjo58994@gmail.com', '1000'],
+            'yjk':['mgjo58995@gmail.com', '1000'],
+            'jocho':['mgjo58969@gmail.com', '1000'],
+            'lee':['mgjo58997@gmail.com', '1000'],
+            'park':['mgjo58989@gmail.com', '1000'],
+            'mkc':['mgjo58999@gmail.com', '1000']
         }
 
 for name in users:
