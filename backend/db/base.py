3a47069a0f0183e483839a3aeb2c586d4f51f5a6
mgjo5899 <mgjo5899@gmail.com>
2018-07-02 00:17:23 -0500

    with MYSQL_HOST env var, we can now access MySQL container from the host and from another container

--- backend/db/base.py
+++ backend/db/base.py
@@ -7,15 +7,20 @@ def get_db_url():
     if 'MYSQL_USER' not in os.environ:
         print("Please set an environment variable MYSQL_USER")
         exit(1)
-    
-    user = os.environ['MYSQL_USER']
 
-    if 'MYSQL_PASSWORD' in os.environ:
+    if 'MYSQL_PASSWORD' not in os.environ:
         print("Please set an environment variable MYSQL_PASSWORD")
         exit(1)
 
-    pw = os.environ['MYSQL_PW']
-    DB_URL =  "mysql+pymysql://{}:{}@localhost/whoamiproject".format(user, pw)
+    if 'MYSQL_HOST' not in os.environ:
+        print("Please set an environment variable MYSQL_HOST")
+        exit(1)
+
+    user = os.environ['MYSQL_USER']
+    pw = os.environ['MYSQL_PASSWORD']
+    db_host = os.environ['MYSQL_HOST']
+
+    DB_URL =  "mysql+pymysql://{}:{}@{}/whoamiproject".format(user, pw, db_host)
 
     return DB_URL
 
