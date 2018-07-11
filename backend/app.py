9e378746a94576f2023dffa2770b2c2b307e65c0
mgjo5899 <mgjo5899@gmail.com>
2018-07-10 23:18:37 -0500

    gmail credential change; enable activate.html in templates

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mgjo5899'
-app.config['MAIL_PASSWORD'] = 'whalsrb6745'
+app.config['MAIL_USERNAME'] = 'whoamiapp2580'
+app.config['MAIL_PASSWORD'] = 'gnsalswjddma2580'
 
 mail = Mail(app)
 CORS(app)
