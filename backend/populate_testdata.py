c6dd1c08d3e399347aa445165c641faab96d709c
mgjo5899 <mgjo5899@gmail.com>
2018-07-17 23:50:22 -0500

    Added graceful close on db in populate_testdata.py

--- backend/populate_testdata.py
+++ backend/populate_testdata.py
@@ -22,3 +22,6 @@ for name in users:
     print(name, users[name])
     user = users[name]
     print(manage.register_user(name, user[1], user[0]))
+    print(manage.confirm_email(user[0]))
+
+utils.db_close()
