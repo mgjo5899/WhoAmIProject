abbbd669763fa318cd2b8674649c09d51ac13fa4
mgjo5899 <mgjo5899@gmail.com>
2018-08-12 11:48:28 +0900

    Moving to datetime.now()

--- backend/whoami_back/models/user.py
+++ backend/whoami_back/models/user.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer, String, Boolean, DateTime
-import time
+from datetime import datetime
 
 from whoami_back.models.base import Base
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -11,6 +12,6 @@ class User(Base):
     email = Column(String(250), unique=True, nullable=False)
     username = Column(String(250), unique=True, nullable=False)
     password = Column(String(250), nullable=False)
-    registered_on = Column(DateTime, nullable=False, default=time.strftime('%Y-%m-%d %H:%M:%S'))
-    confirmed = Column(Boolean, nullable=False, default=False)
+    registered_on = Column(DateTime, default=datetime.now())
+    confirmed = Column(Boolean, default=False)
     confirmed_on = Column(DateTime, nullable=True)
