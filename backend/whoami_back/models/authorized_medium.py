be073ac30c1a4659b20993324157763339b4bebf
mgjo5899 <mgjo5899@gmail.com>
2018-08-12 11:50:16 +0900

    Adding AuthorizedMedium table

---
+++ backend/whoami_back/models/authorized_medium.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy import PrimaryKeyConstraint
+from sqlalchemy.types import TIMESTAMP
+from datetime import datetime
+
+from whoami_back.models.base import Base
+from whoami_back.models.user import User
+
+
+class AuthorizedMedium(Base):
+    __tablename__ = 'authorized_medium'
+    __table_args__ = (PrimaryKeyConstraint('email', 'medium'),)
+
+    email = Column(String(250), ForeignKey(User.email), nullable=False)
+    medium = Column(String(250), nullable=False)
+    authorized_time = Column(DateTime, default=datetime.now())
