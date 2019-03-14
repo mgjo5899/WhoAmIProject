5550ea329e726013eedb1da7272662c3e0dbcff4
mgjo5899 <mgjo5899@gmail.com>
2019-03-13 22:12:22 -0500

    Updated InstagramData after adding WhitebaordData

--- backend/whoami_back/models/instagram_data.py
+++ backend/whoami_back/models/instagram_data.py
@@ -4,24 +4,20 @@ from sqlalchemy.types import TIMESTAMP
 from datetime import datetime
 
 from whoami_back.models.base import Base
-from whoami_back.models.user import User
+from whoami_back.models.whiteboard_data import WhiteboardData
 
 
 class InstagramData(Base):
     __tablename__ = 'instagram_data'
     
     # Unmodifiable information
-    id = Column(String(250), primary_key=True, nullable=False)
-    email = Column(String(250), ForeignKey(User.email), nullable=False)
-    raw_image_url = Column(String(300), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    whiteboard_data_id = Column(Integer, ForeignKey(WhiteboardData.id), nullable=False)
+    raw_content_url = Column(String(300), nullable=False)
     instagram_url = Column(String(300), nullable=False)
     orig_width = Column(Integer, nullable=False)
     orig_height = Column(Integer, nullable=False)
 
     # Modifiable information
-    width = Column(Integer, nullable=True)
-    height = Column(Integer, nullable=True)
-    pos_x = Column(Integer, nullable=True)
-    pos_y = Column(Integer, nullable=True)
-    angle = Column(Integer, nullable=True)
-    last_modified = Column(DateTime, default=datetime.now())
+    curr_width = Column(Integer, nullable=True)
+    curr_height = Column(Integer, nullable=True)
