bfeb92cff717f0a4dda5cfd76c56fbf820eaa75f
mgjo5899 <mgjo5899@gmail.com>
2019-03-27 01:21:30 -0500

    Adding one more user in populate_testdata to test difference in multiple users

--- backend/whoami_back/populate_testdata.py
+++ backend/whoami_back/populate_testdata.py
@@ -6,13 +6,17 @@ utils.reset_db()
 
 # Populating fake user table
 users = {
-            'someuser':['mgjo5899@gmail.com', '1000']
+            'someuser':['mgjo5899@gmail.com', '1000'],
+            'someotheruser':['mcho14@illinois.edu', '1000']
         }
 
 for name in users:
+    print('')
+    print("Populating a user")
     print(name, users[name])
     user = users[name]
     print(manage.register_user(name, user[1], user[0]))
     print(manage.confirm_email(user[0]))
+    print('')
 
 utils.db_close()
