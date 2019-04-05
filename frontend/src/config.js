d6735690216c61c476f4bb6e55d7496c549f76ab
Junwon Jung <junwon3329@gmail.com>
2019-04-05 15:23:10 -0500

    Added color scheme setting for social medias

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,7 @@
 export const SERVER = 'http://localhost:8000';
 export const CODE_REDIRECT_URI = SERVER + '/instagram/get_access_token';
 export const SECRET_KEY = "gnsalswjddma"; // 훈민정음
+export const SOCIAL_MEDIA_COLOR = {
+    'instagram': '#C13584',
+    'facebook': '#3C5A99'
+}
