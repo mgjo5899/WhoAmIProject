77a699c10054fe74ec48a99e8d48253fca2edfa5
Junwon Jung <junwon3329@gmail.com>
2019-04-26 23:12:49 -0500

    Isolated the component limiting to form functions to its javascript file

--- frontend/src/components/profile/form.js
+++ frontend/src/components/profile/form.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { SERVER } from '../../config';
 
-const Form = ({ handleChange, auth, next }) => {
+const Form = ({ profile, setProfile, auth, next }) => {
+
+    const [filled, setFilled] = useState(false);
+
+    useEffect(() => {
+        if (!filled) {
+            setExistingProfileData()
+                .then(() => {
+                    const formDOM = document.getElementById('profile-form');
+                    console.log(formDOM);
+                    setFilled(true);
+                });
+        }
+    }, [profile.data]);
+
+    const handleSave = async e => {
+        e.preventDefault();
+        console.log(profile)
+        try {
+            const { status, message } = (await Axios.post(SERVER + '/user/profile', profile.data)).data;
+            if (!status) throw new Error(message);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const setExistingProfileData = async () => {
+        const profileData = (await Axios.get(SERVER + '/user/profile')).data;
+        setProfile(profile => ({
+            ...profile,
+            data: {
+                ...profile.data,
+                ...profileData.profile
+            }
+        }));
+    }
+
+    const handleChange = e => {
+        e.persist();
+        setProfile(profile => ({
+            ...profile,
+            data: {
+                ...profile.data,
+                [e.target.id]: e.target.value
+            }
+        }));
+    }
 
     return (
-        <form onChange={handleChange}>
+        <form id="profile-form" onChange={handleChange}>
             <div className="form-group">
                 <label htmlFor="bio">Bio</label>
                 <textarea className="form-control" id="bio" placeholder="Bio" />
@@ -24,7 +72,8 @@ const Form = ({ handleChange, auth, next }) => {
                 <label htmlFor="website">Website</label>
                 <input type="text" className="form-control" id="website" placeholder="Website" />
             </div>
-            <button type="submit" className="btn btn-primary mb-2" onClick={next}>Edit profile</button>
+            <button type="submit" className="btn btn-primary m-2" onClick={handleSave}>Save</button>
+            <button type="submit" className="btn btn-primary m-2" onClick={next}>Spread profile</button>
         </form>
     );
 }
