14ae2570ade05dad9ad5bf7b3f86b32dbc662e1a
Junwon Jung <junwon3329@gmail.com>
2019-04-27 17:14:17 -0500

    Added modal to ensure the end users that it has been saved correctly, and also added another button that moves to the whiteboard

--- frontend/src/components/profile/form.js
+++ frontend/src/components/profile/form.js
@@ -1,80 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, Fragment } from 'react';
 import Axios from 'axios';
 import { SERVER } from '../../config';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
 
-const Form = ({ profile, setProfile, auth, next }) => {
+const Form = ({ profileData, setProfileData, auth, next }) => {
 
-    const [filled, setFilled] = useState(false);
+    const [modal, setModal] = useState(false);
 
-    useEffect(() => {
-        if (!filled) {
-            setExistingProfileData()
-                .then(() => {
-                    const formDOM = document.getElementById('profile-form');
-                    console.log(formDOM);
-                    setFilled(true);
-                });
-        }
-    }, [profile.data]);
+    const [message, setMessage] = useState('');
+
+    const toggle = () => {
+        setModal(!modal);
+    }
 
     const handleSave = async e => {
         e.preventDefault();
-        console.log(profile)
         try {
-            const { status, message } = (await Axios.post(SERVER + '/user/profile', profile.data)).data;
+            const { status, message } = (await Axios.post(SERVER + '/user/profile', profileData)).data;
             if (!status) throw new Error(message);
+            setMessage(message);
+            toggle();
         } catch (error) {
             console.log(error);
         }
     }
 
-    const setExistingProfileData = async () => {
-        const profileData = (await Axios.get(SERVER + '/user/profile')).data;
-        setProfile(profile => ({
-            ...profile,
-            data: {
-                ...profile.data,
-                ...profileData.profile
-            }
-        }));
-    }
-
     const handleChange = e => {
         e.persist();
-        setProfile(profile => ({
-            ...profile,
-            data: {
-                ...profile.data,
-                [e.target.id]: e.target.value
-            }
+        setProfileData(profileData => ({
+            ...profileData,
+            [e.target.id]: e.target.value
         }));
     }
 
     return (
-        <form id="profile-form" onChange={handleChange}>
-            <div className="form-group">
-                <label htmlFor="bio">Bio</label>
-                <textarea className="form-control" id="bio" placeholder="Bio" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="company">Company</label>
-                <input type="text" className="form-control" id="company" placeholder="Company" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="location">Location</label>
-                <input type="text" className="form-control" id="location" placeholder="Location" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" readOnly value={auth.user.email} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="website">Website</label>
-                <input type="text" className="form-control" id="website" placeholder="Website" />
-            </div>
-            <button type="submit" className="btn btn-primary m-2" onClick={handleSave}>Save</button>
-            <button type="submit" className="btn btn-primary m-2" onClick={next}>Spread profile</button>
-        </form>
+        <Fragment>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalBody>
+                    <h5>{message}</h5>
+                </ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-primary" onClick={toggle}>Confirm</button>
+                </ModalFooter>
+            </Modal>
+            <form id="profile-form" className="w-50 mx-auto" onChange={handleChange}>
+                <div className="form-group">
+                    <label htmlFor="bio">Bio</label>
+                    <textarea className="form-control" id="bio" placeholder="Bio" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="company">Company</label>
+                    <input type="text" className="form-control" id="company" placeholder="Company" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="location">Location</label>
+                    <input type="text" className="form-control" id="location" placeholder="Location" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" id="email" readOnly value={auth.user.email} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="website">Website</label>
+                    <input type="text" className="form-control" id="website" placeholder="Website" />
+                </div>
+                <button type="submit" className="btn btn-primary m-2" onClick={handleSave}>Save</button>
+                <button type="button" className="btn btn-primary m-2" onClick={next}>Spread profile</button>
+            </form>
+        </Fragment>
     );
 }
 
