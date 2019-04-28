f7d1d25d882efa966725096c9e4f2ee5da179126
Junwon Jung <junwon3329@gmail.com>
2019-04-27 19:20:41 -0500

    Fixed showImages function, argument close --> flag, 0 --> dashboard, 1 --> image spread, 2 --> profile spread, which was added in more conditions to handle

--- frontend/src/store/actions/data_actions.js
+++ frontend/src/store/actions/data_actions.js
@@ -16,61 +16,74 @@ export const setData = data => ({
  * 
  * @param {list} imageData - image data to show up
  * @param {function} clickFunc - operate function when click
- * @param {boolean} close - whether it has close button or not
+ * @param {int} flag - 0 ==> dashboard 1 ==> image spread 2 ==> profile spread
  */
-export const showImages = (imageData, clickFunc, close) => (
-    imageData.map((image, index) => (
-        <div
-            id={image.id}
-            medium={image.medium}
-            orig_width={image.orig_width}
-            orig_height={image.orig_height}
-            className={"card position-absolute rounded" + (close ? ' draggable resize-drag' : '')}
-            key={index}
-            style={{
-                width: image.curr_width || 200,
-                height: 'auto',
-                WebkitTransform: `translate(${image.posX || image.pos_x}px, ${image.posY || image.pos_y}px)`,
-                transform: `translate(${image.posX || image.pos_x}px, ${image.posY || image.pos_y}px)`,
-                background: SOCIAL_MEDIA_CONFIG.find(socialMedia => socialMedia.medium === image.medium) ?
-                    SOCIAL_MEDIA_CONFIG.find(socialMedia => socialMedia.medium === image.medium).backgroundBorderColor :
-                    '#000',
-                padding: 3
-            }}
-            data-x={image.posX || image.pos_x}
-            data-y={image.posY || image.pos_y}
-            onClick={!close ? (() => clickFunc(image)) : undefined}
-        >
-            {close && (
-                <button type="button" onClick={() => clickFunc(image)} className="close position-absolute" style={{ top: '2%', right: '2%' }} aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            )}
-            {
-                image.medium !== 'profile' ? (
-                    <img
-                        className="w-100 h-100"
-                        src={image.src || image.raw_content_url}
-                        alt=""
-                    />
-                ) : (
-                        <div className="card w-100 h-100">
-                            <h1>Profile</h1>
-                        </div>
-                    )
-            }
-        </div>
-    ))
-)
+export const showImages = (imageData, clickFunc, flag) => {
+
+    return imageData.map((image, index) => {
+
+        let className = 'card position-absolute rounded';
+
+        const correctFlag = (flag === 1 && image.medium !== 'whoami') || (flag === 2 && image.type === 'profile');
+
+        if ([1, 2].includes(flag)) {
+            className += ' draggable resize-drag';
+        }
+
+        return (
+            <div
+                id={image.id}
+                medium={image.medium}
+                orig_width={image.orig_width}
+                orig_height={image.orig_height}
+                className={className}
+                key={index}
+                style={{
+                    width: image.curr_width || 200,
+                    height: 'auto',
+                    WebkitTransform: `translate(${image.posX || image.pos_x}px, ${image.posY || image.pos_y}px)`,
+                    transform: `translate(${image.posX || image.pos_x}px, ${image.posY || image.pos_y}px)`,
+                    background: SOCIAL_MEDIA_CONFIG.find(socialMedia => socialMedia.medium === image.medium) ?
+                        SOCIAL_MEDIA_CONFIG.find(socialMedia => socialMedia.medium === image.medium).backgroundBorderColor :
+                        '#000',
+                    padding: 3
+                }}
+                data-x={image.posX || image.pos_x}
+                data-y={image.posY || image.pos_y}
+                onClick={flag === 0 ? (() => clickFunc(image)) : undefined}
+            >
+                {
+                    image.type !== 'profile'
+                        ? (
+                            <img
+                                className="w-100 h-100"
+                                src={image.src || image.raw_content_url}
+                                alt=""
+                            />
+                        )
+                        : (
+                            <div className="card w-100 h-100">
+                                <h1>Profile</h1>
+                            </div>
+                        )
+                }
+                {correctFlag && (
+                    <button type="button" onClick={() => clickFunc(image)} className="close position-absolute" style={{ top: '2%', right: '2%' }} aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                )}
+            </div>
+        )
+    })
+}
 
 export const isOwner = (auth, username) => (auth.user.username === username)
 
-export const getExistingImages = (auth, username, history) => async dispatch => {
+export const getExistingImages = (auth, username) => async dispatch => {
     try {
         isOwner(auth, username) ? await getOwnerExistingImages(dispatch) : await getUserExistingImages(username, dispatch);
     } catch (error) {
         console.log(error)
-        history.push(`/error_page?msg=${error}`);
     }
 }
 
