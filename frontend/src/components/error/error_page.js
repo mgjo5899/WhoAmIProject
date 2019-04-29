5aa03e04a33b9fb80e7c2b97deda31dc9d9a4d1f
Junwon Jung <junwon3329@gmail.com>
2019-04-29 17:09:09 -0500

    Error page created, without a message inside of it

--- frontend/src/components/error/error_page.js
+++ frontend/src/components/error/error_page.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
-import queryString from 'query-string';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./css/style.css";
 
-const ErrorPage = props => {
-    const parsed = queryString.parse(props.location.search);
+const ErrorPage = () => {
+
+    document.title = 'Not Found 404';
+
     return (
-        <Fragment>
-            <h1>Page not found</h1>
-            <h3>{parsed.msg}</h3>
-        </Fragment>
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h1>404</h1>
+                </div>
+                <h2>Oops! This Page Could Not Be Found</h2>
+                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+                <Link to="/">Go To Homepage</Link>
+            </div>
+        </div>
     );
 }
 
