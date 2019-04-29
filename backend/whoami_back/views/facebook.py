1bbd374f063ca83c42fc62703329fcea5ec546f3
mgjo5899 <mgjo5899@gmail.com>
2019-04-29 02:59:11 -0500

    Code refactoring; applying changes in get_whiteboard_data function

--- backend/whoami_back/views/facebook.py
+++ backend/whoami_back/views/facebook.py
@@ -8,17 +8,16 @@ import whoami_back.manage as manage
 
 
 facebook = Blueprint('facebook', __name__)
+MEDIUM = 'facebook'
+
 
 def access_token_api(code):
     rtn_val = {}
     uri = facebook_conf.ACCESS_TOKEN_ENDPOINT
-
-    payload = {
-                'client_id'     : facebook_conf.CLIENT_ID,
+    payload = { 'client_id'     : facebook_conf.CLIENT_ID,
                 'client_secret' : facebook_conf.CLIENT_SECRET,
                 'redirect_uri'  : facebook_conf.ACCESS_TOKEN_REDIRECT_URI,
-                'code'          : code
-              }
+                'code'          : code }
 
     r = requests.post(uri, data=payload)
 
@@ -34,11 +33,9 @@ def access_token_api(code):
 def get_user_contents(access_token):
     rtn_val = {}
     uri = facebook_conf.USER_PHOTOS_ENDPOINT
-    payload = {
-                'access_token': access_token,
+    payload = { 'access_token': access_token,
                 'fields': facebook_conf.FIELDS,
-                'type': facebook_conf.TYPE
-              }
+                'type': facebook_conf.TYPE }
     r = requests.get(uri, payload)
     
     while True:
@@ -56,7 +53,7 @@ def get_user_contents(access_token):
                 rtn_val['status'] = True
                 break
         else:
-            rtn_val = {'status': False}
+            rtn_val = { 'status': False }
             rtn_val['error_message'] = json.loads(r.text)
             break
 
@@ -67,7 +64,7 @@ def refine_raw_data(raw_contents_data):
 
     for raw_content in raw_contents_data:
         content = {}
-        content['facebook_url'] = raw_content['link']
+        content['content_url'] = raw_content['link']
         content['orig_width'] = raw_content['images'][0]['width']
         content['orig_height'] = raw_content['images'][0]['height']
         content['raw_content_url'] = raw_content['images'][0]['source']
@@ -87,49 +84,55 @@ def refine_raw_data(raw_contents_data):
 def get_user_facebook_data():
     rtn_val = {}
 
-    if 'email' in session:
-        access_token = manage.get_medium_access_token(session['email'], 'facebook')
+    if 'email' not in session:
+        rtn_val['status'] = False
+        rtn_val['message'] = "Could not find the user email in the session cookie"
+    else:
+        access_token = manage.get_medium_access_token(session['email'], MEDIUM)
 
-        if access_token['status'] == True:
+        if access_token['status'] != True:
+            rtn_val['status'] = False
+            rtn_val['message'] = "Could not find Instagram access token for the user with the given email"
+        else:
             raw_user_data = get_user_contents(access_token['access_token'])
 
-            if raw_user_data['status'] == True:
+            if raw_user_data['status'] != True:
+                rtn_val = raw_user_data
+                rtn_val['message'] = "Could not get user's Facebook contents"
+            else:
                 # Refine the data
                 refined_contents = refine_raw_data(raw_user_data['user_contents'])
-                existing_facebook_contents = manage.get_whiteboard_data(email=session['email'], medium='facebook')['whiteboard_data']
+                existing_contents = manage.get_whiteboard_data(email=session['email'], medium=MEDIUM)['whiteboard_data']
 
-                # Check if there's any unavailable contents and if there are, mark their status as 3
+                # Check if there's any unavailable contents and if there are, 
+                # mark them unavailable (3)
                 # Also check if there's any existing used (1) or unused (2) content
                 # If there is, retrieve necessary information
-                for existing_content in existing_facebook_contents:
+                for existing_content in existing_contents:
                     available = False
 
                     for curr_content in refined_contents:
-                        if existing_content['raw_content_url'] == curr_content['raw_content_url']:
+                        if existing_content['specifics']['raw_content_url'] == \
+                                            curr_content['raw_content_url']:
                             available = True
                             curr_content['id'] = existing_content['id']
                             curr_content['pos_x'] = existing_content['pos_x']
                             curr_content['pos_y'] = existing_content['pos_y']
                             curr_content['last_modified'] = existing_content['last_modified']
-                            curr_content['curr_width'] = existing_content['curr_width']
-                            curr_content['curr_height'] = existing_content['curr_height']
                             curr_content['status'] = existing_content['status']
+                            curr_content['curr_width'] = \
+                                               existing_content['specifics']['curr_width']
+                            curr_content['curr_height'] = \
+                                               existing_content['specifics']['curr_height']
                             break
+
                     if available == False:
                         marking_result = manage.mark_content_unavailable(existing_content['id'])
                         print(marking_result)
+
                 rtn_val['status'] = True
-                rtn_val['facebook_contents'] = refined_contents
+                rtn_val['contents'] = refined_contents
                 rtn_val['email'] = session['email']
-            else:
-                rtn_val = raw_user_data
-                rtn_val['message'] = "Could not get user's Facebook contents"
-        else:
-            rtn_val['status'] = False
-            rtn_val['message'] = "Could not find Instagram access token for the user with the given email"
-    else:
-        rtn_val['status'] = False
-        rtn_val['message'] = "Could not find the user email in the session cookie"
 
     return jsonify(rtn_val)
 
@@ -138,43 +141,36 @@ def get_facebook_access_token():
     rtn_val = {}
     req = utils.get_req_data()
 
-    if 'code' in req and 'state' in req:
-        if req['state'] == facebook_conf.STATE:
-            rtn_val = access_token_api(req['code'])
-
-            if rtn_val['status'] == True:
-                if 'email' in session:
-                    access_token_data = rtn_val['access_token_data']
-                    rtn_val = {'status': True}
-                    rtn_val['access_token'] = access_token_data['access_token']
-                else:
-                    rtn_val = {'status': False}
-                    rtn_val['message'] = "Could not find the user email in the session cookie"
-                    rtn_val['error_code'] = 3
-            else:
-                rtn_val['error_code'] = 2
-        else:
-            rtn_val['status'] = False
-            rtn_val['message'] = "Failed to get the correct state"
-            rtn_val['error_message'] = req
-            rtn_val['error_code'] = 5
-    else:
+    if 'email' not in session:
+        rtn_val['status'] = False
+        rtn_val['message'] = "Could not find the user email in the session cookie"
+        rtn_val['error_code'] = 3
+    elif not ('code' in req and 'state' in req):
         rtn_val['status'] = False
         rtn_val['message'] = "Failed to get code"
         rtn_val['error_message'] = req
         rtn_val['error_code'] = 1
-
-    if rtn_val['status'] == True:
-        rtn_val = manage.register_medium('facebook', session['email'], rtn_val['access_token'])
+    elif req['state'] != facebook_conf.STATE:
+        rtn_val['status'] = False
+        rtn_val['message'] = "Failed to get the correct state"
+        rtn_val['error_message'] = req
+        rtn_val['error_code'] = 5
+    else:
+        rtn_val = access_token_api(req['code'])
 
         if rtn_val['status'] == False:
-            rtn_val['error_code'] = 4
+            rtn_val['error_code'] = 2
+        else:
+            access_token = rtn_val['access_token_data']['access_token']
+            rtn_val = manage.register_medium(MEDIUM, session['email'], access_token)
 
-    notification = '{}?medium={}&status={}'.format(
-                                                    facebook_conf.OAUTH_RESULT_ENDPOINT,
-                                                    'facebook',
-                                                    str(rtn_val['status']).lower()
-                                                  )
+            if rtn_val['status'] == False:
+                rtn_val['error_code'] = 4
+
+    notification = '{}?medium={}&status={}'.format(facebook_conf.OAUTH_RESULT_ENDPOINT, \
+                                                   MEDIUM, \
+                                                   str(rtn_val['status']).lower())
+
     if rtn_val['status'] == False:
         notification += '&errorcode={}'.format(rtn_val['error_code'])
 
--- backend/whoami_back/views/instagram.py
+++ backend/whoami_back/views/instagram.py
@@ -8,24 +8,19 @@ import whoami_back.manage as manage
 
 
 instagram = Blueprint('instagram', __name__)
+MEDIUM = 'instagram'
 
 
 def access_token_api(code):
     rtn_val = {}
     uri = instagram_conf.ACCESS_TOKEN_ENDPOINT
-    payload = {
-                'client_id'     : instagram_conf.CLIENT_ID,
+    payload = { 'client_id'     : instagram_conf.CLIENT_ID,
                 'client_secret' : instagram_conf.CLIENT_SECRET,
                 'grant_type'    : instagram_conf.GRANT_TYPE,
                 'redirect_uri'  : instagram_conf.ACCESS_TOKEN_REDIRECT_URI,
-                'code'          : code
-              }
+                'code'          : code }
 
     r = requests.post(uri, data=payload)
-
-    # When things are not working
-    #rtn_val['status'] = True
-    #rtn_val['access_token_data'] = json.loads(''.join([t.strip() for t in open('instagram_data.txt', 'r').readlines()]))
 
     if r.status_code == 200:
         rtn_val['status'] = True
@@ -52,47 +47,35 @@ def get_user_contents(access_token):
     return rtn_val
 
 def refine_raw_data(raw_contents_data):
+    raw_contents = []
     contents = []
 
+    # Go through and fill up raw_contents with raw data
     for raw_content in raw_contents_data:
+        if raw_content['type'] == 'carousel':
+            for raw_content in raw_content['carousel_media']:
+                raw_contents.append(raw_content)
+        else:
+            raw_contents.append(raw_content)
+
+    # Refine raw data
+    for raw_content in raw_contents:
+        content = {}
+
         if raw_content['type'] == 'image':
-            # Get standard_resolution
-            content = {}
             content['orig_width'] = raw_content['images']['standard_resolution']['width']
             content['orig_height'] = raw_content['images']['standard_resolution']['height']
             content['raw_content_url'] = raw_content['images']['standard_resolution']['url']
-            content['instagram_url'] = raw_content['link']
+            content['content_url'] = raw_content['link']
             content['type'] = raw_content['type']
-            contents.append(content)
-        elif raw_content['type'] == 'carousel':
-            for carousel_content in raw_content['carousel_media']:
-                if carousel_content['type'] == 'image':
-                    content = {}
-                    content['orig_width'] = carousel_content['images']['standard_resolution']['width']
-                    content['orig_height'] = carousel_content['images']['standard_resolution']['height']
-                    content['raw_content_url'] = carousel_content['images']['standard_resolution']['url']
-                    content['instagram_url'] = raw_content['link']
-                    content['type'] = carousel_content['type']
-                    contents.append(content)
-                elif carousel_content['type'] == 'video':
-                    content = {}
-                    content['orig_width'] = carousel_content['videos']\
-                                                            ['standard_resolution']['width']
-                    content['orig_height'] = carousel_content['videos']\
-                                                             ['standard_resolution']['height']
-                    content['raw_content_url'] = carousel_content['videos']\
-                                                                 ['standard_resolution']['url']
-                    content['instagram_url'] = raw_content['link']
-                    content['type'] = carousel_content['type']
-                    contents.append(content)
         elif raw_content['type'] == 'video':
-            content = {}
             content['orig_width'] = raw_content['videos']['standard_resolution']['width']
             content['orig_height'] = raw_content['videos']['standard_resolution']['height']
             content['raw_content_url'] = raw_content['videos']['standard_resolution']['url']
-            content['instagram_url'] = raw_content['link']
+            content['content_url'] = raw_content['link']
             content['type'] = raw_content['type']
-            contents.append(content)
+
+        contents.append(content)
 
     return contents
 
@@ -100,89 +83,88 @@ def refine_raw_data(raw_contents_data):
 def get_user_instagram_data():
     rtn_val = {}
 
-    if 'email' in session:
-        access_token = manage.get_medium_access_token(session['email'], 'instagram')
+    if 'email' not in session:
+        rtn_val['status'] = False
+        rtn_val['message'] = "Could not find the user credentials"
+    else:
+        access_token = manage.get_medium_access_token(session['email'], MEDIUM)
 
-        if access_token['status'] == True:
+        if access_token['status'] != True:
+            rtn_val['status'] = False
+            rtn_val['message'] = "Could not find Instagram access token for the user with the given email"
+        else:
             raw_user_data = get_user_contents(access_token['access_token'])
 
-            if raw_user_data['status'] == True:
+            if raw_user_data['status'] != True:
+                rtn_val = raw_user_data
+                rtn_val['message'] = "Could not get user's Instagram contents"
+            else:
                 # Refine the data
                 refined_contents = refine_raw_data(raw_user_data['user_contents']['data'])
-                existing_instagram_contents = manage.get_whiteboard_data(email=session['email'], medium='instagram')['whiteboard_data']
+                existing_contents = manage.get_whiteboard_data(email=session['email'], medium=MEDIUM)['whiteboard_data']
 
-                # Check if there's any unavailable contents and if there are, mark their status as 3
+                # Check if there's any unavailable contents and if there are, 
+                # mark them unavailable (3)
                 # Also check if there's any existing used (1) or unused (2) content
                 # If there is, retrieve necessary information
-                for existing_content in existing_instagram_contents:
+                for existing_content in existing_contents:
                     available = False
 
                     for curr_content in refined_contents:
-                        if existing_content['raw_content_url'] == curr_content['raw_content_url']:
+                        if existing_content['specifics']['raw_content_url'] == \
+                                            curr_content['raw_content_url']:
                             available = True
                             curr_content['id'] = existing_content['id']
                             curr_content['pos_x'] = existing_content['pos_x']
                             curr_content['pos_y'] = existing_content['pos_y']
                             curr_content['last_modified'] = existing_content['last_modified']
-                            curr_content['curr_width'] = existing_content['curr_width']
-                            curr_content['curr_height'] = existing_content['curr_height']
                             curr_content['status'] = existing_content['status']
+                            curr_content['curr_width'] = \
+                                               existing_content['specifics']['curr_width']
+                            curr_content['curr_height'] = \
+                                               existing_content['specifics']['curr_height']
                             break
 
                     if available == False:
                         marking_result = manage.mark_content_unavailable(existing_content['id'])
                         print(marking_result)
+
                 rtn_val['status'] = True
-                rtn_val['instagram_contents'] = refined_contents
+                rtn_val['contents'] = refined_contents
                 rtn_val['email'] = session['email']
-            else:
-                rtn_val = raw_user_data
-                rtn_val['message'] = "Could not get user's Instagram contents"
-        else:
-            rtn_val['status'] = False
-            rtn_val['message'] = "Could not find Instagram access token for the user with the given email"
-    else:
-        rtn_val['status'] = False
-        rtn_val['message'] = "Could not find the user email in the session cookie"
 
     return jsonify(rtn_val)
 
 @instagram.route('/instagram/get_access_token', methods=['GET'])
-def get_instagrram_access_token():
+def get_instagram_access_token():
     rtn_val = {}
     req = utils.get_req_data()
 
-    if 'code' in req:
-        rtn_val = access_token_api(req['code'])
-
-        if rtn_val['status'] == True:
-            if 'email' in session:
-                access_token_data = rtn_val['access_token_data']
-                rtn_val = { 'status' : True }
-                rtn_val['access_token'] = access_token_data['access_token']
-            else:
-                rtn_val = { 'status' : False }
-                rtn_val['message'] = "Could not find the user email in the session cookie"
-                rtn_val['error_code'] = 3
-        else:
-            rtn_val['error_code'] = 2
-    else:
+    if 'email' not in session:
+        rtn_val['status'] = False
+        rtn_val['message'] = "Could not find the user email in the session cookie"
+        rtn_val['error_code'] = 3
+    elif 'code' not in req:
         rtn_val['status'] = False
         rtn_val['message'] = "Failed to get code"
         rtn_val['error_message'] = req
         rtn_val['error_code'] = 1
-
-    if rtn_val['status'] == True:
-        rtn_val = manage.register_medium('instagram', session['email'], rtn_val['access_token'])
+    else:
+        rtn_val = access_token_api(req['code'])
 
         if rtn_val['status'] == False:
-            rtn_val['error_code'] = 4
+            rtn_val['error_code'] = 2
+        else:
+            access_token = rtn_val['access_token_data']['access_token']
+            rtn_val = manage.register_medium(MEDIUM, session['email'], access_token)
 
-    notification = '{}?medium={}&status={}'.format(
-                                                    instagram_conf.OAUTH_RESULT_ENDPOINT,
-                                                    'instagram',
-                                                    str(rtn_val['status']).lower()
-                                                  )
+            if rtn_val['status'] == False:
+                rtn_val['error_code'] = 4
+
+    notification = '{}?medium={}&status={}'.format(instagram_conf.OAUTH_RESULT_ENDPOINT, \
+                                                   MEDIUM, \
+                                                   str(rtn_val['status']).lower())
+
     if rtn_val['status'] == False:
         notification += '&errorcode={}'.format(rtn_val['error_code'])
 
