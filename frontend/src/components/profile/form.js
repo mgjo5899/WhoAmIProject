7645b549c5b0b8a584607a81c419d79b6445d715
Junwon Jung <junwon3329@gmail.com>
2019-05-01 15:39:36 -0500

    Created another input_form.js file to make use of showing up the toggle in dashboard.js

--- frontend/src/components/profile/form.js
+++ frontend/src/components/profile/form.js
@@ -1,22 +1,29 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import Axios from 'axios';
 import { SERVER } from '../../config';
 import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+import InputForm from './input_form';
 
-const Form = ({ profileData, setProfileData, auth, next }) => {
+const Form = ({ profile, setProfile, auth, next }) => {
 
     const [modal, setModal] = useState(false);
-
     const [message, setMessage] = useState('');
 
     const toggle = () => {
         setModal(!modal);
     }
 
+    useEffect(() => {
+        const profileForm = document.getElementById('profile-form');
+        for (const key in profile) {
+            if (profileForm[key]) profileForm[key].value = profile[key];
+        }
+    }, [profile]);
+
     const handleSave = async e => {
         e.preventDefault();
         try {
-            const { status, message } = (await Axios.post(SERVER + '/user/profile', profileData)).data;
+            const { status, message } = (await Axios.post(SERVER + '/user/profile', profile)).data;
             if (!status) throw new Error(message);
             setMessage(message);
             toggle();
@@ -27,10 +34,21 @@ const Form = ({ profileData, setProfileData, auth, next }) => {
 
     const handleChange = e => {
         e.persist();
-        setProfileData(profileData => ({
-            ...profileData,
-            [e.target.id]: e.target.value
-        }));
+        if (e.target.id === 'show-email') {
+            console.log('printhere')
+            setProfile({
+                include_email: true
+            });
+        } else if (e.target.id === 'hide-email') {
+            setProfile({
+                include_email: false
+            });
+        } else {
+            setProfile({
+                [e.target.id]: e.target.value
+            });
+        }
+        console.log(profile)
     }
 
     return (
@@ -44,26 +62,7 @@ const Form = ({ profileData, setProfileData, auth, next }) => {
                 </ModalFooter>
             </Modal>
             <form id="profile-form" className="w-50 mx-auto" onChange={handleChange}>
-                <div className="form-group">
-                    <label htmlFor="bio">Bio</label>
-                    <textarea className="form-control" id="bio" placeholder="Bio" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="company">Company</label>
-                    <input type="text" className="form-control" id="company" placeholder="Company" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="location">Location</label>
-                    <input type="text" className="form-control" id="location" placeholder="Location" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" id="email" readOnly value={auth.user.email} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="website">Website</label>
-                    <input type="text" className="form-control" id="website" placeholder="Website" />
-                </div>
+                <InputForm auth={auth} />
                 <button type="submit" className="btn btn-primary m-2" onClick={handleSave}>Save</button>
                 <button type="button" className="btn btn-primary m-2" onClick={next}>Spread profile</button>
             </form>
---
+++ frontend/src/components/profile/input_form.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useEffect } from 'react';
+
+const InputForm = ({ auth, readOnly, profile }) => {
+
+    useEffect(() => {
+        if (readOnly) {
+            for (const key in profile) {
+                const element = document.getElementById(key);
+                if (element) {
+                    if (!profile[key]) {
+                        element.parentElement.style.display = 'none';
+                    } else {
+                        element.value = profile[key];
+                        element.readOnly = true;
+                    }
+                }
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(profile);
+    }, [profile])
+
+    return (
+        <Fragment>
+            <div className="form-group">
+                <label htmlFor="bio">Bio</label>
+                <textarea className="form-control" id="bio" placeholder="Bio" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="company">Company</label>
+                <input type="text" className="form-control" id="company" placeholder="Company" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="location">Location</label>
+                <input type="text" className="form-control" id="location" placeholder="Location" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" className="form-control" id="email" readOnly value={auth.user.email} />
+            </div>
+            <div className="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="show-email" name="email-checked" className="custom-control-input" checked />
+                <label className="custom-control-label" htmlFor="show-email">Show email</label>
+            </div>
+            <div className="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="hide-email" name="email-checked" className="custom-control-input" />
+                <label className="custom-control-label" htmlFor="hide-email">Hide email</label>
+            </div>
+            <div className="form-group">
+                <label htmlFor="website">Website</label>
+                <input type="text" className="form-control" id="website" placeholder="Website" />
+            </div>
+        </Fragment>
+    );
+}
+
+export default InputForm;
