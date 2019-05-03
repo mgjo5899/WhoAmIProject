29e1fd2cac3c7822b59067484308e965a1fb4e7d
Junwon Jung <junwon3329@gmail.com>
2019-05-03 16:40:00 -0500

    Editted error page non selectable

--- frontend/src/components/error/error_page.js
+++ frontend/src/components/error/error_page.js
@@ -6,15 +6,19 @@ const ErrorPage = () => {
 
     document.title = 'Not Found 404';
 
+    const style = {
+        userSelect: 'none'
+    };
+
     return (
         <div id="notfound">
             <div className="notfound">
                 <div className="notfound-404">
-                    <h1>404</h1>
+                    <h1 style={style}>404</h1>
                 </div>
-                <h2>Oops! This Page Could Not Be Found</h2>
-                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-                <Link to="/">Go To Homepage</Link>
+                <h2 style={style}>Oops! This Page Could Not Be Found</h2>
+                <p style={style}>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+                <Link to="/" style={style}>Go To Homepage</Link>
             </div>
         </div>
     );
