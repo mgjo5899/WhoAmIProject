fe720e71604c441fa7e0757d34e02753b8d243bb
mgjo5899 <mgjo5899@gmail.com>
2019-05-29 00:21:58 +0900

    Adding follow test data populating in populate_testdata

--- backend/whoami_back/populate_testdata.py
+++ backend/whoami_back/populate_testdata.py
@@ -11,16 +11,21 @@ users = {
             'user3':['user3@gmail.com', '1000']
         }
 
-for name in users:
+for username in users:
     print('')
     print("Populating a user")
-    print(name, users[name])
-    user = users[name]
-    print(manage.register_user(name, user[1], user[0]))
+    print(username, users[username])
+    user = users[username]
+    print(manage.register_user(username, user[1], user[0]))
     print(manage.confirm_email(user[0]))
     print('')
 
-utils.db_close()
+print('')
+print("Adding follow relationship")
+for follower in users:
+    for followed_user in users:
+        if follower != followed_user:
+            print(manage.add_follower(follower, followed_user))
+            print('')
 
-# TODO: Create functions for testing out each of the following listed item:
-#           1. Follower relationship
+utils.db_close()
