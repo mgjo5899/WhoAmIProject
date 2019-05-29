4b3d2e82d7ac6d1ea42f363130efed8653966372
Junwon Jung <junwon3329@gmail.com>
2019-05-29 15:16:27 -0500

    Basic setting up of followers and following users page have completed, realtime deletion

--- frontend/src/components/follow/following_followers.js
+++ frontend/src/components/follow/following_followers.js
@@ -1,7 +1,53 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Infinite from 'react-infinite';
+import { getFollowersFollowingUsers } from '../../store/actions/data_actions';
+import { SECRET_KEY, SERVER } from '../../config';
+import Axios from 'axios';
+import AnonymousUser from '../../images/anonymous/anonymous_user.png';
 
-const FollowingFollowers = ({ next }) => {
+
+const FollowingFollowers = ({ next, activeIndex, contentsIndex }) => {
+
+    const [followersList, setFollowersList] = useState([]);
+    const [followingUsersList, setFollowingUsersList] = useState([]);
+
+    useEffect(() => {
+        // if active index matches with contents index
+        if (activeIndex === contentsIndex.following_followers) {
+            setUpFollowersFollwingUsers();
+        }
+    }, []);
+
+    const setUpFollowersFollwingUsers = async () => {
+        try {
+            // from data action file, get followers and following users to use
+            const { followers, followingUsers } = await getFollowersFollowingUsers();
+            // make axios call
+            const followersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
+                secret_key: SECRET_KEY,
+                usernames: followers
+            })).data;
+            // handle if status is false
+            if (!followersProfileImages.status) throw new Error(followersProfileImages.message);
+            setFollowersList([...followersProfileImages.follow_relationships]);
+            // do same stuff for following users
+            // from data action file, get followers and following users to use
+            // make axios call
+            const followingUsersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
+                secret_key: SECRET_KEY,
+                usernames: followingUsers
+            })).data;
+            // handle if status is false
+            if (!followingUsersProfileImages.status) throw new Error(followingUsersProfileImages.message);
+            setFollowingUsersList([...followingUsersProfileImages.follow_relationships]);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        console.log(followingUsersList, followersList);
+    }, [followingUsersList, followersList])
 
     // const [followersInfiniteLoading, setFollowersInfiniteLoading] = useState(false);
 
@@ -17,6 +63,59 @@ const FollowingFollowers = ({ next }) => {
         // setTimeout(() => setFollowersInfiniteLoading(false), 3000);
     }
 
+    const followersFollowingUsersRender = followList => (
+        followList.map((follow, index) => (
+            <div key={index} style={{ height: 100 }} className="border row m-2">
+                <div className="col-3 p-0">
+                    <img className="m-2 rounded-circle" src={follow.profile_image_url ? follow.profile_image_url : AnonymousUser} style={{ height: 80 }} alt="" />
+                </div>
+                <div className="col-6 p-0 text-center my-auto" style={{ fontSize: 30 }}>{follow.username}</div>
+                <div className="col-3 p-0 h-50 my-auto">
+                    <button className={"btn d-block float-right mr-2 border" + (follow.following ? ' btn-light' : ' btn-primary')} onClick={() => handleFollowUnfollow(follow)}>
+                        {follow.following ? 'following' : 'follow'}
+                    </button>
+                </div>
+            </div>
+        ))
+    );
+
+    // combine two functions into one
+    const handleFollowUnfollow = async user => {
+        // if user is not following
+        try {
+            if (!user.following) {
+                await handleFollowUser(user);
+            } else {
+                await handleUnfollowUser(user);
+            }
+            setUpFollowersFollwingUsers();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleFollowUser = async user => {
+        // if it is currently not following, follow the user
+        const { status, message } = (await Axios.post(SERVER + '/user/following_users', {
+            followed_user_username: user.username
+        })).data;
+        // if status is false, throw error to print to console output
+        if (!status) throw new Error(message);
+        user.following = true;
+    }
+
+    const handleUnfollowUser = async user => {
+        // if it is currently not following, follow the user
+        const { status, message } = (await Axios.delete(SERVER + '/user/following_users', {
+            data: {
+                followed_user_username: user.username
+            }
+        })).data;
+        // if status is false, throw error to print to console output
+        if (!status) throw new Error(message);
+        user.following = false;
+    }
+
     return (
         <Fragment>
             <div className="row">
@@ -29,24 +128,7 @@ const FollowingFollowers = ({ next }) => {
                             infiniteLoadBeginEdgeOffset={200}
                             onInfiniteLoad={handleFollowersInfiniteLoad}
                         >
-                            <div style={{ height: 100 }} className="border row m-2">
-                                <div className="col-3 border-right p-0">image</div>
-                                <div className="col-6 border-right p-0">username</div>
-                                <div className="col-3 p-0">
-                                    <button className="btn btn-outline-primary d-block w-100 h-50">follow</button>
-                                    <button className="btn btn-outline-primary d-block w-100 h-50">following</button>
-                                </div>
-                            </div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
+                            {followersFollowingUsersRender(followersList)}
                         </Infinite>
                     </div>
                 </div>
@@ -59,17 +141,7 @@ const FollowingFollowers = ({ next }) => {
                             infiniteLoadBeginEdgeOffset={200}
                             onInfiniteLoad={handleFollowingInfiniteLoad}
                         >
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
-                            <div style={{ height: 100 }}>Haha</div>
+                            {followersFollowingUsersRender(followingUsersList)}
                         </Infinite>
                     </div>
                 </div>
