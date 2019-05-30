d3161b9aea32100200bdf707a8d0feaf11302621
Junwon Jung <junwon3329@gmail.com>
2019-05-30 02:09:55 -0500

    Navigate props into followingfollowersdisplay.js component

--- frontend/src/components/follow/following_followers.js
+++ frontend/src/components/follow/following_followers.js
@@ -2,11 +2,11 @@ import React, { Fragment } from 'react';
 import FollowingFollowersDisplay from './following_followers_display';
 
 
-const FollowingFollowers = ({ next, activeIndex, contentsIndex }) => {
+const FollowingFollowers = ({ next, activeIndex, contentsIndex, auth, username }) => {
 
     return (
         <Fragment>
-            <FollowingFollowersDisplay {...{ activeIndex, contentsIndex }} />
+            <FollowingFollowersDisplay {...{ activeIndex, contentsIndex, auth, username }} />
             <div className="d-flex justify-content-center mt-3">
                 <button type="submit" className="btn btn-primary m-2">save</button>
                 <button type="button" className="btn btn-primary m-2" onClick={next}>publish</button>
