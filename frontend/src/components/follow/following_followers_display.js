5f40cb4732a3ade4e579f00473cfef3b6ddc16a2
Junwon Jung <junwon3329@gmail.com>
2019-06-03 03:21:09 -0500

    Handling non-user followers following users calling, and at the same time, dashboard methods and normal followingfollowers methods

--- frontend/src/components/follow/following_followers_display.js
+++ frontend/src/components/follow/following_followers_display.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Axios from 'axios';
 import AnonymousUser from '../../images/anonymous/anonymous_user.png';
 import Infinite from 'react-infinite';
@@ -33,23 +33,34 @@ const FollowingFollowersDisplay = ({ activeIndex, contentsIndex, auth, username,
             const followers = await getFollowers(username);
             const followingUsers = await getFollowingUsers(username);
             // make axios call
-            const followersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
-                secret_key: SECRET_KEY,
-                usernames: followers
-            })).data;
-            // handle if status is false
-            if (!followersProfileImages.status) throw new Error(followersProfileImages.message);
-            setFollowersList([...followersProfileImages.follow_relationships]);
+            // check the credential, if it is logged in, call follow_relationships api
+            if (auth.user.username) {
+                const followersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
+                    secret_key: SECRET_KEY,
+                    usernames: followers
+                })).data;
+                // handle if status is false
+                if (!followersProfileImages.status) throw new Error(followersProfileImages.message);
+                setFollowersList([...followersProfileImages.follow_relationships]);
+            } else {
+                // if it is not logged in, manually put the data inside of followersList
+                setFollowersList([...followers]);
+            }
             // do same stuff for following users
             // from data action file, get followers and following users to use
             // make axios call
-            const followingUsersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
-                secret_key: SECRET_KEY,
-                usernames: followingUsers
-            })).data;
-            // handle if status is false
-            if (!followingUsersProfileImages.status) throw new Error(followingUsersProfileImages.message);
-            setFollowingUsersList([...followingUsersProfileImages.follow_relationships]);
+            // check the credential, if it is logged in, call follow_relationships api
+            if (auth.user.username) {
+                const followingUsersProfileImages = (await Axios.post(SERVER + '/user/follow_relationships', {
+                    secret_key: SECRET_KEY,
+                    usernames: followingUsers
+                })).data;
+                // handle if status is false
+                if (!followingUsersProfileImages.status) throw new Error(followingUsersProfileImages.message);
+            } else {
+                // if it is not logged in, manually put the data inside of followingUsersList
+                setFollowingUsersList([...followingUsers]);
+            }
         } catch (error) {
             console.log(error);
         }
@@ -63,13 +74,17 @@ const FollowingFollowersDisplay = ({ activeIndex, contentsIndex, auth, username,
                     <img className="m-2 rounded-circle" src={follow.profile_image_url ? follow.profile_image_url : AnonymousUser} style={{ height: 80 }} alt="" />
                 </div>
                 <div className="col-6 p-0 text-center my-auto" style={{ fontSize: 30, userSelect: 'none' }} onClick={() => window.open(follow.username)}>{follow.username}</div>
-                <div className={"col-3 p-0 h-50 my-auto" + (follow.following === undefined ? ' d-none' : '')}>
-                    <button className={"btn float-right mr-2 border" +
-                        (follow.following ? ' btn-light' : ' btn-primary') +
-                        (follow.username === auth.user.username ? ' d-none' : ' d-block')} onClick={() => handleFollowUnfollow(follow)}>
-                        {follow.following ? 'following' : 'follow'}
-                    </button>
-                </div>
+                {
+                    auth.user.username && (
+                        <div className="col-3 p-0 h-50 my-auto">
+                            <button className={"btn float-right mr-2 border" +
+                                (follow.following ? ' btn-light' : ' btn-primary') +
+                                (follow.username === auth.user.username ? ' d-none' : ' d-block')} onClick={() => handleFollowUnfollow(follow)}>
+                                {follow.following ? 'following' : 'follow'}
+                            </button>
+                        </div>
+                    )
+                }
             </div>
         ))
     );
@@ -111,37 +126,78 @@ const FollowingFollowersDisplay = ({ activeIndex, contentsIndex, auth, username,
         user.following = false;
     }
 
-
+    const mapRender = dashboardFlag => {
+        if (dashboardFlag) {
+            return (
+                <Fragment>
+                    <div className="row">
+                        <div className="col-12 border-right">
+                            <h5>Followers</h5>
+                            <div className="card p-2 followers">
+                                <Infinite
+                                    containerHeight={500}
+                                    elementHeight={100}
+                                    infiniteLoadBeginEdgeOffset={200}
+                                    onInfiniteLoad={handleFollowersInfiniteLoad}
+                                >
+                                    {followersFollowingUsersRender(followersList)}
+                                </Infinite>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-12">
+                            <h5>Following</h5>
+                            <div className="card followers p-2">
+                                <Infinite
+                                    containerHeight={500}
+                                    elementHeight={100}
+                                    infiniteLoadBeginEdgeOffset={200}
+                                    onInfiniteLoad={handleFollowingInfiniteLoad}
+                                >
+                                    {followersFollowingUsersRender(followingUsersList)}
+                                </Infinite>
+                            </div>
+                        </div>
+                    </div>
+                </Fragment>
+            )
+        } else {
+            return (
+                <div className="row">
+                    <div className="col-sm-6 border-right">
+                        <h5>Followers</h5>
+                        <div className="card p-2 followers">
+                            <Infinite
+                                containerHeight={500}
+                                elementHeight={100}
+                                infiniteLoadBeginEdgeOffset={200}
+                                onInfiniteLoad={handleFollowersInfiniteLoad}
+                            >
+                                {followersFollowingUsersRender(followersList)}
+                            </Infinite>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <h5>Following</h5>
+                        <div className="card followers p-2">
+                            <Infinite
+                                containerHeight={500}
+                                elementHeight={100}
+                                infiniteLoadBeginEdgeOffset={200}
+                                onInfiniteLoad={handleFollowingInfiniteLoad}
+                            >
+                                {followersFollowingUsersRender(followingUsersList)}
+                            </Infinite>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
 
     return (
-        <div className="row">
-            <div className="col-sm-6 border-right">
-                <h5>Followers</h5>
-                <div className="card p-2 followers">
-                    <Infinite
-                        containerHeight={500}
-                        elementHeight={100}
-                        infiniteLoadBeginEdgeOffset={200}
-                        onInfiniteLoad={handleFollowersInfiniteLoad}
-                    >
-                        {followersFollowingUsersRender(followersList)}
-                    </Infinite>
-                </div>
-            </div>
-            <div className="col-sm-6">
-                <h5>Following</h5>
-                <div className="card followers p-2">
-                    <Infinite
-                        containerHeight={500}
-                        elementHeight={100}
-                        infiniteLoadBeginEdgeOffset={200}
-                        onInfiniteLoad={handleFollowingInfiniteLoad}
-                    >
-                        {followersFollowingUsersRender(followingUsersList)}
-                    </Infinite>
-                </div>
-            </div>
-        </div>
+        mapRender(dashboard)
     );
 }
 
